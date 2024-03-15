5b131cbd077371954de27f3c7294d1e873965b9a
Asfer Ali <120978706+Asfer-dev@users.noreply.github.com>
2024-03-15 15:11:55 +0500

    Time: 0 ms (100.00%) | Memory: 16.3 MB (42.69%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        int highest = 0;
-        int m = 1;
-        for (int i = 0; i < nums.size(); i++) {
+        int highest = -11;
+        int m = 1, m2 = 1;
+        for (int i = 0, j = nums.size() - 1; i < nums.size(), j > -1; i++, j--) {
             m *= nums[i];
+            m2 *= nums[j];
             if (m > highest) {
                 highest = m;
+            }
+            if (m2 > highest) {
+                highest = m2;
             }
             if (m == 0) {
                 m = 1;
             }
-        }
-        m = 1;
-        for (int j = nums.size() - 1; j > -1; j--) {
-            m *= nums[j];
-            if (m > highest) {
-                highest = m;
-            }
-            if (m == 0) {
-                m = 1;
+            if (m2 == 0) {
+                m2 = 1;
             }
         }
+        // m = 1;
+        // for (int j = nums.size() - 1; j > -1; j--) {
+        //     m *= nums[j];
+        //     if (m > highest) {
+        //         highest = m;
+        //     }
+        //     if (m == 0) {
+        //         m = 1;
+        //     }
+        // }
         return highest;
     }
 };
