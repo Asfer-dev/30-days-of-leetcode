a5205b1815a0f84d59c5b9d16de7ab159633d185
Asfer Ali <120978706+Asfer-dev@users.noreply.github.com>
2024-03-15 14:32:29 +0500

    Time: 16 ms (38.61%) | Memory: 20.4 MB (58.33%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,23 +1,16 @@
-#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // int n = nums.size();
-        // int sum = 0;
-        // for (int i = 0; i < n; i++) {
-        //     sum += nums[i];
-        // }
-        // return ((n * (n + 1)) / 2) - sum;
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        return ((n * (n + 1)) / 2) - sum;
 
-        int sum=0, n=nums.size(); 
-        for (int i=0 ; i<n; i++)
-            sum^=i^nums[i];
-        return sum^n;
+        // int sum=0, n=nums.size(); 
+        // for (int i=0 ; i<n; i++)
+        //     sum^=i^nums[i];
+        // return sum^n;
     }
 };
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
