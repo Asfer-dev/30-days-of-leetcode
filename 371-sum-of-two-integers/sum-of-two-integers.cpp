bb35727b28cc0cab8ddf287fb7d6d5f69aa14592
Asfer Ali <120978706+Asfer-dev@users.noreply.github.com>
2024-03-19 21:22:36 +0500

    Time: 0 ms (100.00%) | Memory: 7.2 MB (22.41%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int getSum(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
         bool addition = true;
         bool neg = false;
         int av = a;
