246cce06b0b34302562409f56176edf95ff72b41
Asfer Ali <120978706+Asfer-dev@users.noreply.github.com>
2024-04-05 07:58:16 +0500

    Time: 13 ms (26.10%) | Memory: 8.4 MB (82.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length()) return false;
-        unordered_map<char, int> map;
-        for (int i = 0; i < s.length(); i++) {
-            map[s[i]]++;
-        }
-        for (int i = 0; i < t.length(); i++) {
-            map[t[i]]--;
-        }
-        for (auto& [chr, count]: map) {
-            if (map[chr] != 0) return false;
-        }
-        return true;
+        // unordered_map<char, int> map;
+        // for (int i = 0; i < s.length(); i++) {
+        //     map[s[i]]++;
+        // }
+        // for (int i = 0; i < t.length(); i++) {
+        //     map[t[i]]--;
+        // }
+        // for (auto& [chr, count]: map) {
+        //     if (map[chr] != 0) return false;
+        // }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
