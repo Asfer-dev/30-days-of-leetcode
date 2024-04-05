8e5ee5db5338022324bbb83ece3a81d05f0fcfc1
Asfer Ali <120978706+Asfer-dev@users.noreply.github.com>
2024-04-05 09:30:52 +0500

    Time: 10 ms (12.04%) | Memory: 16.5 MB (5.04%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -17,21 +17,24 @@ public:
         vector<vector<int>> tree1(tree.begin(), tree.begin() + height(root));
         return tree1;
     }
-    void re(TreeNode* root, int height, vector<vector<int>>& tree) {
+    int re(TreeNode* root, int height, vector<vector<int>>& tree) {
         if (root == NULL)
-            return;
+            return 0;
         tree[height].push_back(root -> val);
         if (root -> left != NULL)
             re(root -> left, height+1, tree);
         if (root -> right != NULL) {
             re(root -> right, height+1, tree);
         }
+        return height++;
     }
     int height(TreeNode* root) {
     if (root == nullptr) {
-        return 0;
+        return 0; // Height of an empty tree is -1
     } else {
-        return 1 + max(height(root->left), height(root->right));
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        return 1 + max(leftHeight, rightHeight);
     }
 }
 };
